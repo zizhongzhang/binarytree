b39a24b8a33fd66f05d0a44efe656c3a912a70bc
Zizhong <zizhong.zhang43@gmail.com>
2018-08-17 23:07:05 +1000

    built base unit test to measure realtime audience qualification performance

--- src/SegmentTester/UnitTest1.cs
+++ src/SegmentTester/UnitTest1.cs
@@ -1,7 +1,9 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -12,14 +14,41 @@ namespace SegmentTester
         private readonly Audience[] _audiences;
         public UnitTests(TestSetup setup)
         {
-            setup.CreateRandomAudience(4,2);
+            setup.CreateRandomAudience(10000, 3);
             _audiences = setup.Audiences;
         }
 
         [Fact]
         public void Test1()
         {
-            var a = "";
+            var stopwatch = new Stopwatch();
+            var signal = new Dictionary<string, string>();
+            signal.Add("orgregion", "au");
+            signal.Add("payGst", "no");
+            stopwatch.Start();
+            var qualifiedAudiences = Validator.Qualify(signal, _audiences);
+            stopwatch.Stop();
+            var milliSeconds = stopwatch.ElapsedMilliseconds;
+            var uniqueNames = qualifiedAudiences.Select(q => q.Name).Distinct().Count();
+            //Console.WriteLine(milliSeconds);
+        }
+    }
+
+    public class Validator
+    {
+        public static Audience[] Qualify(IDictionary<string, string> signals, Audience[] audiences)
+        {
+            var audienceSet = new HashSet<Audience>();
+            foreach (var signal in signals)
+            {
+                var filteredAudiences = audiences.Where(a => a.AttributeKeys.Contains(signal.Key));
+                foreach (var audience in filteredAudiences)
+                {
+                    //skip expression evaluation for now
+                    audienceSet.Add(audience);
+                }
+            }
+            return audienceSet.ToArray();
         }
     }
 
@@ -32,6 +61,8 @@ namespace SegmentTester
         private readonly string[] _textOperators;
         private readonly string[] _numberOperators;
         private readonly string[] _booleanOperators = new[] { "&&", "||" };
+        private readonly string[] _attributeKeys;
+        public IDictionary<string, IList<string>> KeyToTraitsMapping = new Dictionary<string, IList<string>>();
 
         public Audience[] Audiences => _audiences.ToArray();
 
@@ -47,6 +78,7 @@ namespace SegmentTester
             _sampleAttributeUpLimit = _sampleData.SampleSignals.Length;
             _textOperators = _sampleData.TextOperators;
             _numberOperators = _sampleData.NumberOperators;
+            _attributeKeys = _sampleData.SampleSignals.Select(s => s.Key).ToArray();
         }
 
         private SampleSignal GetRandomSignal()
@@ -54,7 +86,7 @@ namespace SegmentTester
             return _sampleData.SampleSignals[_random.Next(_sampleAttributeUpLimit)];
         }
 
-        private string GetRandomTrait()
+        private (string expression,string attributeKey) GetRandomTrait()
         {
             var attribute = GetRandomSignal();
             var valueIndex = _random.Next(attribute.Values.Length);
@@ -71,24 +103,46 @@ namespace SegmentTester
                 sb.Append(@operator);
                 sb.Append(attribute.Values[valueIndex]);
             }
-            return sb.ToString();
+            var expression = sb.ToString();
+            return (expression, attribute.Key);
+        }
+
+        private void BuildUpMapping(string key, string expression)
+        {
+            if (KeyToTraitsMapping.ContainsKey(key))
+            {
+                KeyToTraitsMapping[key].Add(expression);
+            }
+            else
+            {
+                KeyToTraitsMapping.Add(key, new List<string> { expression });
+            }
+        }
+
+        private bool HasBracket()
+        {
+            return _random.Next(100) >= 49;
         }
 
         public void CreateRandomAudience(int numberOfAudience, int maximumTraits)
         {
             for (var i = 0; i < numberOfAudience; i++)
             {
+                var audience = new Audience();
                 var traitsLimit = _random.Next(maximumTraits);
                 var sb = new StringBuilder();
                 for (var j = 0; j <= traitsLimit; j++)
                 {
-                    sb.Append(GetRandomTrait());
+                    var trait = GetRandomTrait();
+                    audience.AttributeKeys.Add(trait.attributeKey);
+                    sb.Append(trait.expression);
                     if (j < traitsLimit)
                     {
                         sb.Append(_booleanOperators[_random.Next(_booleanOperators.Length)]);
                     }
                 }
-                _audiences.Add(new Audience(sb.ToString()));
+                audience.Expression = sb.ToString();
+                _audiences.Add(audience);
             }
         }
     }
@@ -107,6 +161,11 @@ namespace SegmentTester
 
     public class Audience
     {
+        public Audience()
+        {
+            Name = Guid.NewGuid().ToString();
+            AttributeKeys = new List<string>();
+        }
         public Audience(string exp)
         {
             Expression = exp;
@@ -118,6 +177,7 @@ namespace SegmentTester
         }
         public string Name { get; set; }
         public string Expression { get; set; }
+        public IList<string> AttributeKeys { get; set; }
     }
 
     class SampleData
